5c48b9cae17af73998a3945f943edcea09f9c261
crstinapl <acpleiro@gmail.com>
2018-07-29 21:07:41 +0200

    first js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('>> Ready :)');
+console.log(input);
+var input = document.querySelectorAll('.radio');
+
+var button = document.querySelector('.first');
+var body = document.querySelector ('body');
+var main = document.querySelector('main');
+
+
+var numberOfCards;
+console.log(numberOfCards);
+for (var i =0; i<input.length; i++){
+  input[i].addEventListener('click', start);
+}
+
+function start(event){
+  numberOfCards = event.currentTarget.value;
+}
