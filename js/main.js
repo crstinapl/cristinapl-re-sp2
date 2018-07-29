8edc17dec197eb0f4c6e46ba7ff7405bed495a20
crstinapl <acpleiro@gmail.com>
2018-07-29 21:38:18 +0200

    created front-card

--- js/main.js
+++ js/main.js
@@ -8,6 +8,37 @@ var body = document.querySelector ('body');
 var main = document.querySelector('main');
 
 
+function getCard() {
+
+  var url='https://raw.githubusercontent.com/Adalab/cards-data/master/';
+  var urlBack ='https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
+
+  fetch (url+numberOfCards+'.json')
+    .then(function(response){
+      return response.json();
+
+    })
+    .then (function(json){
+      console.log(json);
+      main.innerHTML='';
+
+      var list = document.createElement('ul');
+      list.classList.add('grid');
+      main.appendChild(list);
+      for ( var j = 0; j<json.length; j++){
+        var card = document.createElement('li');
+        card.classList.add('cardFront');
+        list.appendChild(card);
+
+        var image = document.createElement('img');
+        card.appendChild(image);
+        image.src= urlBack;
+      }
+
+    });
+}
+
+
 var numberOfCards;
 console.log(numberOfCards);
 for (var i =0; i<input.length; i++){
@@ -17,3 +48,6 @@ for (var i =0; i<input.length; i++){
 function start(event){
   numberOfCards = event.currentTarget.value;
 }
+
+
+button.addEventListener('click', getCard);
